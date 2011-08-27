0b8b22ae521963096f8578d08c396e99ec7f5f86
Kenneth Reitz <me@kennethreitz.com>
2011-08-27 13:44:36 -0400

    core structure

---
+++ envoy/core.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""
+envoy.core
+~~~~~~~~~~
+
+This module provides
+"""
+
+import subprocess
+import shlex
+
+
+class Response(object):
+    """A command's response"""
+
+    def __init__(self):
+        super(Response, self).__init__()
+        self.command = None
+
+    def __repr__(self):
+        return '<Respones [{0}]>'.format(self.command)
+
+    @property
+    def status_code(self):
+        return 0
+
+    @property
+    def std_out(self):
+        return ''
+
+    @property
+    def std_err(self):
+        return ''
+
+
+def run(command, data=None, timeout=None):
+    """Executes a given commmand and returns Response.
+
+    Blocks until process is complete, or timeout is reached.
+    """
+
+    return Response()
