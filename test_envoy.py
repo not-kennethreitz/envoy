c449de960421fc2e01d08a97e235cc55dde94805
Kenneth Reitz <me@kennethreitz.com>
2011-08-31 00:33:09 -0400

    test => test_envoy

--- test_envoy.py
+++ test_envoy.py
@@ -2,19 +2,19 @@ import unittest
 import envoy
 
 class SimpleTest(unittest.TestCase):
-    
-    def testInput(self):
+
+    def test_input(self):
         r = envoy.run("sed s/i/I/g", "Hi")
-        self.assertEqual(r.std_out, "HI")
+        self.assertEqual(r.std_out.rstrip(), "HI")
         self.assertEqual(r.status_code, 0)
-    
-    def testPipe(self):
+
+    def test_pipe(self):
         r = envoy.run("echo -n 'hi'| tr [:lower:] [:upper:]")
         self.assertEqual(r.std_out, "HI")
         self.assertEqual(r.status_code, 0)
 
-    def testTimeout(self):
-        r = envoy.run("yes | head", timeout=1)
+    def test_timeout(self):
+        r = envoy.run('yes | head', timeout=1)
         self.assertEqual(r.std_out, 'y\ny\ny\ny\ny\ny\ny\ny\ny\ny\n')
         self.assertEqual(r.status_code, 0)
 
