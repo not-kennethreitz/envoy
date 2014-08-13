b2453fe0da1e4386706b5cd25c3d261ce5bc4731
Kenneth Reitz <me@kennethreitz.com>
2014-08-13 11:00:35 -0400

    upload.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 
 if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist upload")
+    os.system("python setup.py sdist bdist_wheel upload")
     sys.exit()
 
 required = []
